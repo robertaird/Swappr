12c7345c1342d73e2e8a746176141e532f4cb509
zacharygagnier <gzachary@gmail.com>
2017-11-06 12:43:16 -0600

    [change] changed default image to be swappr logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="https://i.imgur.com/HyT4LZP.png">
     <router-view/>
   </div>
 </template>
@@ -19,5 +19,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+  width: 400px;
+  height: 400px;
 }
 </style>
