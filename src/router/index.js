4772c18955fd6367b9d9c26b69a8f60383bf86fe
zacharygagnier <gzachary@gmail.com>
2017-11-06 12:43:45 -0600

    [add] created new mainpage.vue with template, pointed router to it

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import loginScreen from '@/components/loginScreen';
+import mainPage from '@/components/mainPage';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld,
+      name: 'Login',
+      component: loginScreen,
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      component: mainPage,
     },
   ],
 });
