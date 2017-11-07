1e9d161fbf1172b940b23c7fb6674393a24e7637
zacharygagnier <gzachary@gmail.com>
2017-11-06 19:26:38 -0600

    [add] dependency for axios, method in loginscreen

--- src/components/loginScreen.vue
+++ src/components/loginScreen.vue
@@ -1,34 +1,37 @@
+const axios = require
+
 <template>
   <div class="hello">
     <img src="https://i.imgur.com/HyT4LZP.png">
     <h1>{{ msg }}</h1>
-    <button @click="nextPage">Log In</button>
+    <button class="btn" @click="nextPage">Log In</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'loginScreen',
+  name: "loginScreen",
   data() {
     return {
-      msg: 'Welcome to Swappr',
+      msg: "Welcome to Swappr"
     };
   },
   methods: {
     nextPage() {
-      this.msg = 'logged in!';
-    },
-  },
+      axios.get("/main");
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 .hello {
-    text-align: center;
+  text-align: center;
 }
 
 img {
