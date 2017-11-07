9f5ed7c907417f05f7eff302f199323053df948d
Rob Aird <emailrobaird@gmail.com>
2017-11-06 22:04:01 -0600

    [change] tradeitem to es6

--- app/migrations/20171107035327-create-trade-item.js
+++ app/migrations/20171107035327-create-trade-item.js
@@ -1,36 +1,34 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('TradeItems', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('TradeItems', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       img_url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('TradeItems');
-  }
+        type: Sequelize.DATE,
+      },
+    }),
+
+  down: queryInterface/* Sequelize */ =>
+    queryInterface.dropTable('TradeItems'),
 };
--- app/models/tradeitem.js
+++ app/models/tradeitem.js
@@ -1,16 +1,15 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  var TradeItem = sequelize.define('TradeItem', {
+  const TradeItem = sequelize.define('TradeItem', {
     description: DataTypes.STRING,
     name: DataTypes.STRING,
     type: DataTypes.STRING,
-    img_url: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+    img_url: DataTypes.STRING,
   });
+
+  TradeItem.associate = (models) => {
+    // associations can be defined here
+  };
+
   return TradeItem;
 };
