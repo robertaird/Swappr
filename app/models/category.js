a26fcde0b9caa1262660f321faf6089ed8d398e3
Rob Aird <emailrobaird@gmail.com>
2017-11-07 15:14:01 -0600

    [add] basic implementation of category schema

---
+++ app/models/category.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    name: DataTypes.STRING,
+  });
+
+  Category.associate = (models) => {
+    // associations can be defined here
+  };
+
+  return Category;
+};
