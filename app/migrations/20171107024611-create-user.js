33b571985380eefc54cb7ccea31e2ca13b7791f1
Rob Aird <emailrobaird@gmail.com>
2017-11-06 21:04:26 -0600

    [add] refactored User migration file to es6

---
+++ app/migrations/20171107024611-create-user.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      facebook: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    }),
+  down: queryInterface /* Sequelize */ =>
+    queryInterface.dropTable('Users'),
+
+};
