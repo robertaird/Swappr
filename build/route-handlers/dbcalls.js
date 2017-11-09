57aed2abd60fe6ab2db31a306d6f324477f143df
Rob Aird <emailrobaird@gmail.com>
2017-11-08 22:02:52 -0600

    [add] successfully placing a user in the database

--- build/route-handlers/dbcalls.js
+++ build/route-handlers/dbcalls.js
@@ -11,7 +11,22 @@ app.post('/items', (req, res) => {
     newItem.id_category = 17;
   }
   console.log(newItem);
-  res.send();
+  db.Item.create(newItem)
+    .then(createdItem =>
+      res.send(createdItem))
+    .catch((err) => {
+      console.log(err);
+      res.send(500, 'something went wrong!')
+    });
+  // res.send();
+});
+
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+  db.User.create({ name: 'Bilbo Baggins', email: 'bilbo@bagend.com', id_facebook: 666 })
+    .then(user => res.send(user))
+    .catch(err => console.log(err));
 });
 
 module.exports = app;
