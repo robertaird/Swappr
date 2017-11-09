3f9aef7b951a372a618413174bd22af22089c734
Rob Aird <emailrobaird@gmail.com>
2017-11-08 23:43:49 -0600

    [add] handles POST request to transactions, checks for existing first and returns updated

---
+++ build/route-handlers/db-transactions.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const db = require('../../app/db');
+
+const app = express();
+
+app.use(express.json());
+
+// app.get('/transactions', (req, res) => {
+
+// });
+const createTransaction = (res, newTransaction) =>
+  db.Transaction.create(newTransaction)
+    .then(createdTransaction =>
+      res.send(createdTransaction))
+    .catch((err) => {
+      console.log(err);
+      res.send(500, 'something went wrong!');
+  });
+
+app.post('/transactions', (req, res) => {
+  const newTransaction = req.body;
+  const { id_item_offered, id_item_desired } = newTransaction;
+  db.Transaction.update(
+    {
+      pending: false,
+      accepted: true,
+    },
+    {
+      where: {
+        id_item_desired: id_item_offered,
+        id_item_offered: id_item_desired,
+      },
+      returning: true,
+    },
+  ).then(([rows, updatedTransaction]) => {
+    if (rows > 0) {
+      res.send(updatedTransaction);
+    } else {
+      createTransaction(res, newTransaction);
+    }
+  }).catch(err =>
+    console.log(err));
+});
+
+module.exports = app;
