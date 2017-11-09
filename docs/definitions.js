ace67572101ea89abfc62c3e37de681d8263cb7f
Rob Aird <emailrobaird@gmail.com>
2017-11-08 22:35:08 -0600

    [add] definitions for calls to /transactions

--- docs/definitions.js
+++ docs/definitions.js
@@ -14,17 +14,39 @@ const user = {
 // * DB Post * //
 // * Basically, anything that is passed around and deals with the database.
 
+// * POST to /users
 const newUser = {
   id_facebook: null,
   email: null,
 };
 
+// * POST to /items
 const newItem = {
   id_user: null,
   description: null,
   name: null,
   id_category: null,
   url_img: null,
+};
+
+// * POST to /transactions
+// * Set up a new Transaction like so:
+const newTransactionWanted = {
+  id_user: 5,
+  id_item_offered: 3,
+  id_item_desired: 2,
+  pending: true,
+};
+
+// * If a user 'swipes left', we can create an entry in the transaction database where pending and
+// * accepted are both FALSE. When we query the database to look for potential new items, we can
+// * avoid showing a user items they've already declined.
+const newTransactionLeftSwipe = {
+  id_user: 5,
+  id_item_offered: 2,
+  id_item_desired: 2,
+  pending: false,
+  accepted: false,
 };
 
 // * Item Categories * //
