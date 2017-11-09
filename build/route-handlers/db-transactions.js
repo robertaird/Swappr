cede82c8731f57ae36c894728fd15c1e6b46aa2a
Rob Aird <emailrobaird@gmail.com>
2017-11-09 01:14:35 -0600

    [add] search parameter to hide items already seen from User

--- build/route-handlers/db-transactions.js
+++ build/route-handlers/db-transactions.js
@@ -17,23 +17,36 @@ const createTransaction = (res, newTransaction) =>
       res.send(500, 'something went wrong!');
     });
 
+const getSeenItems = (userId) =>
+  db.Transaction.findAll({ where: { id_user: userId }, raw: true })
+    .then(items =>
+      items.map(({ id_item_desired: itemId }) => itemId)
+    )
+    .catch(err =>
+      console.log(err));
+
 app.get('/transactions', (req, res) => {
   const { id_user: userId } = req.headers;
-  db.Item.find(
-    {
-      where: {
-        id_user: {
-          [Op.notIn]: [userId],
+  getSeenItems(userId).then((itemIds) => {
+    db.Item.find(
+      {
+        where: {
+          id: {
+            [Op.notIn]: itemIds,
+          },
+          id_user: {
+            [Op.notIn]: [userId],
+          },
         },
       },
-    },
-  ).then((item) => {
-    console.log(item);
-    res.send(item);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.send(500);
+    ).then((item) => {
+      console.log(item);
+      res.send(item);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(500);
+    });
   });
 });
 
