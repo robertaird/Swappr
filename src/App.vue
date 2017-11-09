94ef55d78a7d764cbda35ca460bc4e87fefc270b
zacharygagnier <gzachary@gmail.com>
2017-11-09 00:58:05 -0600

    [fix] re-added css elements to app, removed redundent bootstrap dependency

--- src/App.vue
+++ src/App.vue
@@ -32,8 +32,15 @@ export default {
 </script>
 
 <style>
-@import '../node_modules/bootstrap/dist/css/bootstrap.css';
 .btn-margin {
   margin-top: 7px
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
