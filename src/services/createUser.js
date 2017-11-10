e1dacfd785baa351ec9add48c2e970e738e1f70c
Rob Aird <emailrobaird@gmail.com>
2017-11-09 22:10:35 -0600

    [remove] then statement from post request

--- src/services/createUser.js
+++ src/services/createUser.js
@@ -2,15 +2,6 @@ import axios from 'axios';
 
 export default {
   createUser(newUser) {
-    axios.post('/users', newUser)
-      .then(createdUser => console.log(createdUser));
-  },
-  getUser({ id_facebook }) {
-    const config = {
-      headers: {
-        id_facebook,
-      },
-    };
-    axios.get('/users', config);
+    return axios.post('/users', newUser);
   },
 };
