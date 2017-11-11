32f3dc4961d6290c2353b42e9e78945d21d45f53
Rob Aird <emailrobaird@gmail.com>
2017-11-10 22:08:07 -0600

    [remove] bootstrap.css from main.js - interfered with styling

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App';
 import addItem from './components/addItem.vue';
