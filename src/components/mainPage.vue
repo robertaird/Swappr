ec768a2d6678eaa82d07bb6747cce713662906c0
Rob Aird <robkaird@gmail.com>
2017-12-19 09:30:05 -0600

    (refactor) button layout in navbar

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -3,22 +3,24 @@
     <nav class="navbar">
       <div class="nav-contents container">
         <div class="row w-100 ">
-          <div class="col-12 col-sm-4 col-md-5">
+          <div class="col-12 col-sm-3 col-md-5">
             <img src="../assets/logo-white.png" class="float-left" style="width: 120px;">
           </div>
-          <div class="col-4 col-sm-3 col-md-2 mx-0">
-            <div style="width: 7em;">
-              <button class="btn btn-test btn-block float-right" @click="profilePage">Profile</button>
+          <div class="col-12 col-sm-9 col-md-7 px-0" align="right">
+            <div style="float: right;">
+              <button class="btn btn-test signout" @click="auth.logout">Sign Out</button>
             </div>
-          </div>
-          <div class="col-5 col-sm-3 col-md-3 px-0">
-            <span class="fa-stack fa-5x has-badge ml-auto" :data-count="tradeOffers.length">
-              <button class="btn btn-test ml-auto pending-btn float-right" @click="tradeView">Pending Trades</button>
-              <pending-trades ref="pendingTrades" v-bind="$props" :tradeOffers='tradeOffers'></pending-trades>
-            </span>
-          </div>
-          <div class="col-3 col-sm-2 col-md-2 mx-0 pr-0">
-            <button class="btn btn-test signout float-right" @click="auth.logout">Sign Out</button>
+            <div style="float: right;">
+              <div class="col-3 px-0">
+                <span class="fa-stack fa-5x has-badge" :data-count="tradeOffers.length">
+                  <button class="btn btn-test pending-btn" @click="tradeView">Pending Trades</button>
+                  <pending-trades ref="pendingTrades" v-bind="$props" :tradeOffers='tradeOffers'></pending-trades>
+                </span>
+              </div>
+            </div>
+            <div style="float: right; width: 7em;">
+              <button class="btn btn-test btn-block" @click="profilePage">Profile</button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,56 +1,35 @@
 <template>
-  <div>
-    <nav class="navbar">
-      <div class="nav-contents container">
-        <div class="row w-100">
-          <div class="col-12 col-sm-3 col-md-5">
-            <img src="../assets/logo-white.png" class="float-left" style="width: 120px;">
-          </div>
-          <div class="col-12 col-sm-9 col-md-7 px-0" align="right">
-            <div style="float: right;">
-              <button class="btn btn-test signout" @click="auth.logout">Sign Out</button>
-            </div>
-            <div style="float: right;">
-              <div class="col-3 px-0">
-                <span class="fa-stack fa-5x has-badge" :data-count="tradeOffers.length">
-                  <button class="btn btn-test pending-btn" @click="tradeView">Pending Trades</button>
-                  <pending-trades ref="pendingTrades" v-bind="$props" :tradeOffers='tradeOffers'></pending-trades>
-                </span>
-              </div>
-            </div>
-            <div style="float: right; width: 7em;">
-              <button class="btn btn-test btn-block" @click="mainMenu">Swap!</button>
-            </div>
-          </div>
+  <nav class="navbar">
+    <div class="nav-contents container">
+      <div class="row w-100">
+        <div class="col-12 col-sm-3 col-md-5">
+          <img src="../assets/logo-white.png" class="float-left" style="width: 120px;">
         </div>
-      </div>
-    </nav>
-    <div class="container main-container">
-      <div class="card inner-container p-2" style="min-height: 10em;">
-        <div class="float-right" style="height: 3rem;"></div>
-        <add-item v-bind="$props" v-on:new-item="newItem"></add-item>
-        <div class="card pl-3 my-1 w-100 item-box">
-          <div class="container">
-            <div class="row">
-              <item-view v-for="(item,index) in profileItems" :item='item' :key='index' v-on:deleted-item="getUserItems"></item-view>
+        <div class="col-12 col-sm-9 col-md-7 px-0" align="right">
+          <div style="float: right;">
+            <button class="btn btn-test signout" @click="auth.logout">Sign Out</button>
+          </div>
+          <div style="float: right;">
+            <div class="col-3 px-0">
+              <span class="fa-stack fa-5x has-badge" :data-count="tradeOffers.length">
+                <button class="btn btn-test pending-btn" @click="tradeView">Pending Trades</button>
+                <pending-trades ref="pendingTrades" v-bind="$props" :tradeOffers='tradeOffers'></pending-trades>
+              </span>
             </div>
+          </div>
+          <div style="float: right; width: 7em;">
+            <button class="btn btn-test btn-block" @click="mainMenu">Swap!</button>
           </div>
         </div>
       </div>
     </div>
-    <nav class="navbar" style="position: absolute; bottom: 0; height: 3em;">
-      <div class="nav-contents container">
-        <h6 class="created-by pt-1">Created by HoneyBadgerHackers</h6>
-      </div>
-    </nav>
-  </div>
+  </nav>
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
-  name: 'profile',
+  // name: 'profile',
   props: ['auth', 'authentication', 'userId'],
   data() {
     return {
